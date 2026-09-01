303b00d6063b639a2c16aa90a1f95cd5fce46da4
KimSeonguk <tjsrnr3180@gmail.com>
2026-09-01 12:30:40 +0900

    feat(boss): GAS와 체력 Attribute 연동

--- RS/Source/RS/Character/Boss/RSBossCharacter.cpp
+++ RS/Source/RS/Character/Boss/RSBossCharacter.cpp
@@ -1,12 +1,18 @@
 #include "RSBossCharacter.h"
 
 #include "GameFramework/CharacterMovementComponent.h"
+#include "RSAbilitySystemComponent.h"
+#include "RSAbilitySet.h"
 #include "RSBossController.h"
 #include "RSBossEncounter.h"
+#include "RSHealthSet.h"
 
 ARSBossCharacter::ARSBossCharacter()
 {
 	PrimaryActorTick.bCanEverTick = false;
+
+	AbilitySystemComp = CreateDefaultSubobject<URSAbilitySystemComponent>(TEXT("RSAbilitySystemComponent"));
+	HealthSet = CreateDefaultSubobject<URSHealthSet>(TEXT("RSHealthSet"));
 
 	// 레벨 배치와 런타임 생성 방식이 달라도 동일한 Controller 초기화 흐름을 사용합니다
 	AIControllerClass = ARSBossController::StaticClass();
@@ -21,6 +27,35 @@ ARSBossCharacter::ARSBossCharacter()
 	CharacterMovementComp->bOrientRotationToMovement = false;
 	CharacterMovementComp->bUseControllerDesiredRotation = true;
 	CharacterMovementComp->RotationRate = FRotator(0.0f, 240.0f, 0.0f);
+}
+
+void ARSBossCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	InitializeAbilitySystem();
+}
+
+UAbilitySystemComponent* ARSBossCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComp;
+}
+
+void ARSBossCharacter::InitializeAbilitySystem()
+{
+	if (!AbilitySystemComp)
+	{
+		return;
+	}
+
+	AbilitySystemComp->InitAbilityActorInfo(this, this);
+
+	if (AbilitySystemComp->IsOwnerActorAuthoritative() && !bDefaultAbilitiesGranted && DefaultAbilitySet)
+	{
+		DefaultAbilitySet->GiveToAbilitySystem(AbilitySystemComp, &GrantedAbilityHandles, this);
+
+		bDefaultAbilitiesGranted = true;
+	}
 }
 
 void ARSBossCharacter::SetBossEncounter(ARSBossEncounter* InBossEncounter)
--- RS/Source/RS/Character/Boss/RSBossCharacter.h
+++ RS/Source/RS/Character/Boss/RSBossCharacter.h
@@ -2,13 +2,17 @@
 
 #include "CoreMinimal.h"
 #include "RSBaseCharacter.h"
+#include "AbilitySystemInterface.h"
+#include "RSAbilitySet.h"
 #include "RSBossCharacter.generated.h"
 
 class ARSBossEncounter;
+class URSAbilitySystemComponent;
+class URSHealthSet;
 
 /** 보스 전용 설정과 Controller 연결의 기반이 되는 캐릭터입니다 */
 UCLASS()
-class RS_API ARSBossCharacter : public ARSBaseCharacter
+class RS_API ARSBossCharacter : public ARSBaseCharacter, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
 
@@ -16,6 +20,50 @@ public:
 	/** 보스가 배치되거나 생성될 때 BossController가 자동으로 빙의하도록 기본값을 구성합니다 */
 	ARSBossCharacter();
 
+protected:
+	/** 보스가 직접 소유한 ASC를 초기화하고 기본 AbilitySet을 부여합니다 */
+	virtual void BeginPlay() override;
+
+#pragma region GAS
+
+public:
+	/** 보스 Character가 직접 소유한 ASC를 반환합니다 */
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+	/** 보스 Character가 직접 소유한 RS ASC를 반환합니다 */
+	URSAbilitySystemComponent* GetRSAbilitySystemComponent() const { return AbilitySystemComp; }
+
+	/** 보스 Character가 직접 소유한 HealthSet을 반환합니다 */
+	const URSHealthSet* GetHealthSet() const { return HealthSet; }
+
+private:
+	/** ASC의 Owner와 Avatar를 보스 Character로 초기화하고 기본 AbilitySet을 한 번 부여합니다 */
+	void InitializeAbilitySystem();
+
+protected:
+	/** 이 보스가 초기화될 때 기본으로 부여할 AbilitySet입니다 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "RS|Abilities")
+	TObjectPtr<URSAbilitySet> DefaultAbilitySet;
+
+private:
+	/** 보스의 Ability와 Gameplay Effect 상태를 소유하는 ASC입니다 */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "RS|Abilities", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<URSAbilitySystemComponent> AbilitySystemComp;
+
+	/** 보스의 체력 Attribute를 ASC와 같은 생명주기로 유지합니다 */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "RS|Health", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<URSHealthSet> HealthSet;
+
+	/** DefaultAbilitySet이 부여한 Ability를 정확히 식별하기 위한 핸들입니다 */
+	UPROPERTY(Transient)
+	FRSAbilitySet_GrantedHandles GrantedAbilityHandles;
+
+	/** 기본 AbilitySet의 중복 부여를 방지합니다 */
+	bool bDefaultAbilitiesGranted = false;
+
+#pragma endregion
+
+public:
 	/** 이 캐릭터의 전투 참가자와 생명주기를 관리할 Encounter를 설정합니다 */
 	void SetBossEncounter(ARSBossEncounter* InBossEncounter);
 
