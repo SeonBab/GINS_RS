8bf49343d955e5a2efd79888297a9e17d81f3f1c
KimSeonguk <tjsrnr3180@gmail.com>
2026-09-02 12:04:51 +0900

    style(gameplaytags): 태그 선언과 정의 순서 정렬
    - 주석 분류를 알파벳 오름차순으로 배치하고 각 분류 내부 태그도 같은 규칙으로 정렬
    - 헤더와 소스의 나열 순서를 일치시켜 누락된 정의를 대조하기 쉽게 함

--- RS/Source/RS/GameplayTags/RSGameplayTags.cpp
+++ RS/Source/RS/GameplayTags/RSGameplayTags.cpp
@@ -5,27 +5,27 @@
 
 namespace RSGameplayTags
 {
-	// Input
-	UE_DEFINE_GAMEPLAY_TAG_COMMENT(InputTag_MoveTo, "InputTag.MoveTo", "Move to the clicked world location");
-	UE_DEFINE_GAMEPLAY_TAG_COMMENT(InputTag_Ability_BasicAttack, "InputTag.Ability.BasicAttack", "");
-	UE_DEFINE_GAMEPLAY_TAG_COMMENT(InputTag_Ability_Dash, "InputTag.Ability.Dash", "대시 Ability 입력을 식별합니다");
-
 	// Ability
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Ability_Combat_BasicAttack, "Ability.Combat.BasicAttack", "플레이어 기본 공격 Ability를 식별합니다");
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Ability_Movement_Dash, "Ability.Movement.Dash", "대시 이동 Ability를 식별합니다");
 
-	// State
-	UE_DEFINE_GAMEPLAY_TAG_COMMENT(State_Action_Locked, "State.Action.Locked", "");
-	UE_DEFINE_GAMEPLAY_TAG_COMMENT(State_Defense_Evading, "State.Defense.Evading", "대미지와 경직에는 면역이지만 넉백은 허용하는 회피 상태입니다");
-	UE_DEFINE_GAMEPLAY_TAG_COMMENT(State_Defense_KnockbackImmune, "State.Defense.KnockbackImmune", "넉백에만 면역인 상태입니다");
-	UE_DEFINE_GAMEPLAY_TAG_COMMENT(State_Defense_Invulnerable, "State.Defense.Invulnerable", "대미지와 모든 적대적 상태이상에 면역인 상태입니다");
-	UE_DEFINE_GAMEPLAY_TAG_COMMENT(State_Dead, "State.Dead", "Character is dead");
-	UE_DEFINE_GAMEPLAY_TAG_COMMENT(State_Movement_Dashing, "State.Movement.Dashing", "대시 이동이 실행되는 동안 적용합니다");
-
 	// Cooldown
-	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Cooldown_Active, "Cooldown.Active", "하나 이상의 Ability 쿨다운이 활성화된 상태를 나타냅니다");
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Cooldown_Ability_Dash, "Cooldown.Ability.Dash", "쿨다운이 끝날 때까지 대시 재활성화를 차단합니다");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Cooldown_Active, "Cooldown.Active", "하나 이상의 Ability 쿨다운이 활성화된 상태를 나타냅니다");
+
+	// Input
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(InputTag_Ability_BasicAttack, "InputTag.Ability.BasicAttack", "");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(InputTag_Ability_Dash, "InputTag.Ability.Dash", "대시 Ability 입력을 식별합니다");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(InputTag_MoveTo, "InputTag.MoveTo", "Move to the clicked world location");
 
 	// SetByCaller
 	UE_DEFINE_GAMEPLAY_TAG_COMMENT(SetByCaller_Cooldown_Duration, "SetByCaller.Cooldown.Duration", "");
+
+	// State
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(State_Action_Locked, "State.Action.Locked", "");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(State_Dead, "State.Dead", "Character is dead");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(State_Defense_Evading, "State.Defense.Evading", "대미지와 경직에는 면역이지만 넉백은 허용하는 회피 상태입니다");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(State_Defense_Invulnerable, "State.Defense.Invulnerable", "대미지와 모든 적대적 상태이상에 면역인 상태입니다");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(State_Defense_KnockbackImmune, "State.Defense.KnockbackImmune", "넉백에만 면역인 상태입니다");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(State_Movement_Dashing, "State.Movement.Dashing", "대시 이동이 실행되는 동안 적용합니다");
 }
--- RS/Source/RS/GameplayTags/RSGameplayTags.h
+++ RS/Source/RS/GameplayTags/RSGameplayTags.h
@@ -6,27 +6,27 @@
 
 namespace RSGameplayTags
 {
-	// Input
-	RS_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_MoveTo);
-	RS_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Ability_BasicAttack);
-	RS_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Ability_Dash);
-
 	// Ability
 	RS_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_Combat_BasicAttack);
 	RS_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_Movement_Dash);
+
+	// Cooldown
+	RS_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Cooldown_Ability_Dash);
+	RS_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Cooldown_Active);
+
+	// Input
+	RS_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Ability_BasicAttack);
+	RS_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Ability_Dash);
+	RS_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_MoveTo);
+
+	// SetByCaller
+	RS_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(SetByCaller_Cooldown_Duration);
 
 	// State
 	RS_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(State_Action_Locked);
 	RS_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(State_Dead);
 	RS_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(State_Defense_Evading);
-	RS_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(State_Defense_KnockbackImmune);
 	RS_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(State_Defense_Invulnerable);
+	RS_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(State_Defense_KnockbackImmune);
 	RS_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(State_Movement_Dashing);
-
-	// Cooldown
-	RS_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Cooldown_Active);
-	RS_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Cooldown_Ability_Dash);
-
-	// SetByCaller
-	RS_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(SetByCaller_Cooldown_Duration);
 }
