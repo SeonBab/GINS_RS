65f9874d2a6917f13d281e6fd293aca20b02c88f
KimSeonguk <tjsrnr3180@gmail.com>
2026-09-07 12:45:36 +0900

    feat(boss): 보스 사망 시 보스전 완료 연결
    - 보스 사망 처리에서 Encounter에 완료를 알려 처치 순간의 남은 시간이 고정되게 함
    - CompleteEncounter는 서버에서만 동작하고 이미 완료된 전투를 무시하므로 사망이 다시 전달되어도 결과가 바뀌지 않음

--- RS/Source/RS/Character/Boss/RSBossCharacter.cpp
+++ RS/Source/RS/Character/Boss/RSBossCharacter.cpp
@@ -98,6 +98,13 @@ void ARSBossCharacter::HandleDeathStarted(URSHealthComponent* InHealthComponent)
 	}
 
 	GetCharacterMovement()->DisableMovement();
+
+	// 처치 순간의 남은 시간을 고정하는 것은 Encounter가 전투 결과를 확정할 때뿐이므로 사망 처리에서 완료를 알립니다
+	// CompleteEncounter는 서버에서만 동작하고 이미 완료된 전투를 무시하므로 사망이 다시 전달되어도 결과가 바뀌지 않습니다
+	if (BossEncounter)
+	{
+		BossEncounter->CompleteEncounter();
+	}
 }
 
 void ARSBossCharacter::SetBossEncounter(ARSBossEncounter* InBossEncounter)
