d9205c69c0fd7d6b45005dc83166ac25a5213e13
KimSeonguk <tjsrnr3180@gmail.com>
2026-09-08 16:17:53 +0900

    feat(game): 메인 메뉴 맵과 GameMode 추가
    - 전투용 Pawn과 HUD를 만들지 않는 최소 GameMode를 추가하고 MainMenuMap에 지정
    - 패키징 대상 맵 목록에 DevMap과 MainMenuMap 등록

---
+++ RS/Source/RS/GameMode/RSMainMenuGameMode.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RSMainMenuGameMode.h"
+
+ARSMainMenuGameMode::ARSMainMenuGameMode()
+{
+	bStartPlayersAsSpectators = true;
+	DefaultPawnClass = nullptr;
+	HUDClass = nullptr;
+}
